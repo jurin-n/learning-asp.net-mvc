22a7a3be694396f009abee2167261d1c2bffc898
jurin-n <jurin.nakano@gmail.com>
2020-09-29 17:29:38 +0900

    Selenium試し始めた。

---
+++ WebApp/WebApp.Tests/Controllers/LoginControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+
+namespace WebApp.Tests.Controllers
+{
+    [TestClass]
+    public class LoginControllerTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            var execPath = AppDomain.CurrentDomain.BaseDirectory;
+            var appRoot = execPath.Substring(0, execPath.IndexOf("bin"));
+            using (IWebDriver driver = new EdgeDriver(appRoot + "selenium_WebDrivers"))
+            //using (IWebDriver driver = new ChromeDriver(appRoot + "selenium_WebDrivers"))
+            {
+                driver.Navigate().GoToUrl(@"https://selenium.dev");
+            }
+        }
+    }
+}
---
+++ WebApp/WebApp.Tests/selenium_WebDrivers/README.md
@@ -0,0 +1,3 @@
+### WebDriver“üŽèŒ³
+- [ChromeDriver](https://chromedriver.storage.googleapis.com/index.html?path=85.0.4183.87/)
+- [EdgeDriver](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)
