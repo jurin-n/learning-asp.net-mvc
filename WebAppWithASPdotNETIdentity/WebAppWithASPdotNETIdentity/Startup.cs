476dc325e401635005e85f3d28087089d6d401d4
jurin-n <jurin.nakano@gmail.com>
2020-10-24 11:51:25 +0900

    ASP.NET Identity プロジェクト作って、Emailとパスワードによる認証からユーザ名とパスワードによる認証にカスタマイズしてみた。

---
+++ WebAppWithASPdotNETIdentity/WebAppWithASPdotNETIdentity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppWithASPdotNETIdentity.Startup))]
+namespace WebAppWithASPdotNETIdentity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
